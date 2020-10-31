83c9755f25fda0d09e6ed4c4fc33cffa3bdcf544
Kumari Aasha <kumariaasha14@gmail.com>
2020-10-30 18:46:09 -0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php 
+ // Connects to Our Database 
+$link = new mysqli('localhost', 'username', 'password', 'db_name');
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    echo "Could not connect to database, please check your connection details";
+    exit();
+}
+
+ ?> 
